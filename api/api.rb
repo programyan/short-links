fd3390b2c2573371e266ea30a2c6e6dd27fc572b
Andrew Ageev <ageev86@gmail.com>
2020-02-03 08:45:56 +0300

    Add constraint to link param

--- api/api.rb
+++ api/api.rb
@@ -5,11 +5,12 @@ require 'securerandom'
 require 'import'
 
 class Api < Grape::API
+  URL_REGEXP = %r{http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+}.freeze
   format :json
 
-  desc 'Create a short link'
+  desc 'Get a short link to the given resource'
   params do
-    requires :link, type: String, desc: 'Link'
+    requires :link, type: String, desc: 'Link to the resource', regexp: URL_REGEXP
   end
   post do
     key = SecureRandom.alphanumeric(10)
